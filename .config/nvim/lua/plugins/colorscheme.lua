988fd1e35c93e35f1422d0045b38165f5a73d6f8
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-08-26 21:51:11 +0530

    changed the pink colors to red

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -31,7 +31,7 @@ return {
         colors.fg_gutter = "#505070"
         colors.fg_sidebar = "#e0e0ff"
 
-        -- Modern color palette
+        -- Modern color palette - PINK REPLACED WITH RED SHADES
         colors.red = "#ff6b6b" -- Bright red
         colors.red1 = "#ff8f8f"
         colors.red2 = "#ff4b4b"
@@ -41,7 +41,7 @@ return {
         colors.cyan = "#00e0e0" -- Electric cyan
         colors.teal = "#20c0c0"
         colors.orange = "#ffa500" -- Pure orange
-        colors.pink = "#ff80ff" -- Electric pink
+        colors.pink = "#ff6b6b" -- Changed from pink to red shade
         colors.white = "#ffffff"
         colors.silver = "#c0c0e0"
 
@@ -52,13 +52,13 @@ return {
 
         -- UI colors
         colors.border = "#404060"
-        colors.border_highlight = "#ff80ff"
+        colors.border_highlight = "#ff6b6b" -- Changed from pink to red
 
         -- Diagnostic colors
         colors.error = "#ff6b6b"
         colors.warning = "#ffa500"
         colors.info = "#00e0e0"
-        colors.hint = "#ff80ff"
+        colors.hint = "#ff6b6b" -- Changed from pink to red
       end,
       on_highlights = function(highlights, colors)
         -- Base
@@ -76,7 +76,7 @@ return {
 
         -- Line numbers
         highlights.LineNr = { fg = colors.fg_gutter }
-        highlights.CursorLineNr = { fg = colors.pink, bold = true }
+        highlights.CursorLineNr = { fg = colors.red, bold = true } -- Changed from pink to red
 
         -- Search
         highlights.Search = { bg = colors.yellow, fg = "#000000", bold = true }
@@ -85,7 +85,7 @@ return {
 
         -- Syntax highlighting - cleaner and more distinct
         highlights.Comment = { fg = "#708090", italic = true } -- Slate gray
-        highlights.Keyword = { fg = colors.pink, bold = true }
+        highlights.Keyword = { fg = colors.red, bold = true } -- Changed from pink to red
         highlights.Function = { fg = colors.cyan, bold = true }
         highlights.String = { fg = colors.yellow }
         highlights.Number = { fg = colors.orange }
@@ -93,11 +93,11 @@ return {
         highlights.Constant = { fg = colors.red }
         highlights.Variable = { fg = colors.fg }
         highlights.Type = { fg = colors.blue }
-        highlights.Operator = { fg = colors.pink }
+        highlights.Operator = { fg = colors.red } -- Changed from pink to red
         highlights.Special = { fg = colors.purple }
         highlights.Identifier = { fg = colors.fg }
         highlights.PreProc = { fg = colors.purple }
-        highlights.Include = { fg = colors.pink }
+        highlights.Include = { fg = colors.red } -- Changed from pink to red
         highlights.Define = { fg = colors.purple }
         highlights.Macro = { fg = colors.purple }
         highlights.StorageClass = { fg = colors.blue, bold = true }
@@ -105,21 +105,21 @@ return {
         highlights.Typedef = { fg = colors.blue }
 
         -- Language specific
-        highlights["@keyword.function"] = { fg = colors.pink, bold = true }
-        highlights["@keyword.return"] = { fg = colors.pink, bold = true }
-        highlights["@keyword.operator"] = { fg = colors.pink }
+        highlights["@keyword.function"] = { fg = colors.red, bold = true } -- Changed from pink to red
+        highlights["@keyword.return"] = { fg = colors.red, bold = true } -- Changed from pink to red
+        highlights["@keyword.operator"] = { fg = colors.red } -- Changed from pink to red
         highlights["@variable.builtin"] = { fg = colors.cyan }
         highlights["@property"] = { fg = colors.green }
         highlights["@parameter"] = { fg = colors.fg }
         highlights["@constructor"] = { fg = colors.blue, bold = true }
-        highlights["@tag"] = { fg = colors.pink }
+        highlights["@tag"] = { fg = colors.red } -- Changed from pink to red
         highlights["@tag.attribute"] = { fg = colors.orange }
-        highlights["@tag.delimiter"] = { fg = colors.pink }
+        highlights["@tag.delimiter"] = { fg = colors.red } -- Changed from pink to red
 
         -- HTML
-        highlights.htmlTag = { fg = colors.pink }
-        highlights.htmlEndTag = { fg = colors.pink }
-        highlights.htmlTagName = { fg = colors.pink }
+        highlights.htmlTag = { fg = colors.red } -- Changed from pink to red
+        highlights.htmlEndTag = { fg = colors.red } -- Changed from pink to red
+        highlights.htmlTagName = { fg = colors.red } -- Changed from pink to red
         highlights.htmlArg = { fg = colors.orange }
         highlights.htmlString = { fg = colors.yellow }
 
@@ -187,11 +187,11 @@ return {
         highlights.NeoTreeGitAdded = { fg = colors.green }
         highlights.NeoTreeGitModified = { fg = colors.yellow }
         highlights.NeoTreeGitDeleted = { fg = colors.red }
-        highlights.NeoTreeRootName = { fg = colors.pink, bold = true }
+        highlights.NeoTreeRootName = { fg = colors.red, bold = true } -- Changed from pink to red
 
         -- Which-key
         highlights.WhichKey = { fg = colors.cyan }
-        highlights.WhichKeyGroup = { fg = colors.pink }
+        highlights.WhichKeyGroup = { fg = colors.red } -- Changed from pink to red
         highlights.WhichKeyDesc = { fg = colors.fg }
         highlights.WhichKeySeperator = { fg = colors.purple }
         highlights.WhichKeyFloat = { bg = colors.bg }
@@ -202,7 +202,7 @@ return {
         highlights.CmpItemAbbrDeprecated = { fg = colors.fg_gutter, strikethrough = true }
         highlights.CmpItemAbbrMatch = { fg = colors.cyan, bold = true }
         highlights.CmpItemAbbrMatchFuzzy = { fg = colors.cyan, bold = true }
-        highlights.CmpItemKind = { fg = colors.pink }
+        highlights.CmpItemKind = { fg = colors.red } -- Changed from pink to red
         highlights.CmpItemMenu = { fg = colors.orange }
 
         -- Special enhancements
